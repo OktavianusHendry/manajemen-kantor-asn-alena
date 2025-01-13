bf0c6b2fe048aa288f93606c8e8193b8e5f8526c
Oktavianus Hendry <oktavianus.hendry@student.pradita.ac.id>
2025-01-13 11:52:47 +0700

    test

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -56,11 +56,11 @@
         <!-- ===============================================-->
         <!--    JavaScripts-->
         <!-- ===============================================-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js" integrity="sha384-oBqDVmMzpN4dSLEjAKC+JoKjDoi7uA6mK3sT1RqkTg48K3sBhB7CVvq46O+8Pz1L" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" integrity="sha384-nseu+W+svFsnGVckVEh30D1Rt+tL2V9GG77pU25zZ4/7WZG5r+/GBJGzZ2zAJ6gH" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/is_js@0.9.0/is.min.js"></script>
         <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" integrity="sha384-v+V6H92tF+nvnG3rGxKe0lFAdnx42UV4jAz0D5CgQ7f5kOG7E8rS3zV46IiqE+2H" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
         <script src="public/asset/js/theme.js"></script>
 
         <link
