811d4c0a35f2af3c408f60c0f80d337b5343e657
Frrama <arjunafadli@gmail.com>
2025-02-12 17:12:04 +0700

    update

--- resources/views/surat-keluar/index.blade.php
+++ resources/views/surat-keluar/index.blade.php
@@ -35,79 +35,52 @@
                             </div>
                         </form>
 
-                        @if ($surat_keluars->count() > 0)
-                            <div class="table-responsive text-nowrap">
-                                <br>
-                                <table class="table table-hover align-content-center">
-                                    <thead>
+                        @if ($surat_keluars ?? false)
+                        <div class="table-responsive text-nowrap">
+                            <br>
+                            <table class="table table-hover align-content-center">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nomor Surat</th>
+                                        <th>Tanggal Surat</th>
+                                        <th>Perihal</th>
+                                        <th>Tujuan Surat</th>
+                                        <th>Status Validasi</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="table-border-bottom-0 align-content-center">
+                                    @foreach ($surat_keluars as $surat_keluar)
                                         <tr>
-                                            <th>No</th>
-                                            <th>Nomor Surat</th>
-                                            <th>Tanggal Surat</th>
-                                            <th>Perihal</th>
-                                            <th>Tujuan Surat</th>
-                                            <th>Status Validasi</th>
-                                            <th>Aksi</th>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $surat_keluar->nomor_surat }}</td>
+                                            <td>{{ $surat_keluar->tanggal_surat }}</td>
+                                            <td>{{ $surat_keluar->perihal }}</td>
+                                            <td>{{ $surat_keluar->tujuan_surat }}</td>
+                                            <td>{{ $surat_keluar->status_validasi }}</td>
+                                            <td>
+                                                <div class="action-buttons">
+                                                    <a href="{{ route('surat_keluar.view', $surat_keluar->id_surat) }}" class="btn btn-success btn-sm">Lihat</a>
+                                                    <a href="{{ route('surat_keluar.edit', $surat_keluar->id_surat) }}" class="btn btn-warning btn-sm">
+                                                        <i class="menu-icon tf-icons bx bx-edit"></i>
+                                                    </a>
+                                                    <!-- Additional action buttons here -->
+                                                </div>
+                                            </td>
                                         </tr>
-                                    </thead>
-                                    <tbody class="table-border-bottom-0 align-content-center">
-                                        @foreach ($surat_keluars as $surat_keluar)
-                                            <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td>{{ $surat_keluar->nomor_surat }}</td>
-                                                <td>{{ $surat_keluar->tanggal_surat }}</td>
-                                                <td>{{ $surat_keluar->perihal }}</td>
-                                                <td>{{ $surat_keluar->tujuan_surat }}</td>
-                                                <td>{{ $surat_keluar->status_validasi }}</td>
-                                                <td>
-                                                    <div class="action-buttons">
-                                                        <a href="{{ route('surat_keluar.view', $surat_keluar->id_surat) }}" class="btn btn-success btn-sm">Lihat</a>
-                                                        <a href="{{ route('surat_keluar.edit', $surat_keluar->id_surat) }}" class="btn btn-warning btn-sm">
-                                                            <i class="menu-icon tf-icons bx bx-edit"></i>
-                                                        </a>
-
-                                                        @if ($surat_keluar->status_validasi == 'Disetujui')
-                                                            <a href="{{ route('surat_keluar.exportPDF', $surat_keluar->id_surat) }}" class="btn btn-primary btn-sm">Unduh Surat</a>
-                                                            
-                                                            <form method="POST" action="{{ route('surat_keluar.validasi', $surat_keluar->id_surat) }}" style="display:inline;">
-                                                                @csrf
-                                                                @method('PUT')
-                                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin membatalkan validasi surat ini?')">
-                                                                    Batalkan Validasi
-                                                                </button>
-                                                            </form>
-                                                        @else
-                                                            <form method="POST" action="{{ route('surat_keluar.destroy', $surat_keluar->id_surat) }}" style="display:inline;">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus surat ini?')">
-                                                                    &nbsp;<i class="menu-icon tf-icons bx bx-trash"></i>
-                                                                </button>
-                                                            </form>
-
-                                                            <form method="POST" action="{{ route('surat_keluar.validasi', $surat_keluar->id_surat) }}" style="display:inline;">
-                                                                @csrf
-                                                                @method('PUT')
-                                                                <button type="submit" class="btn btn-primary btn-sm" onclick="return confirm('Apakah Anda yakin ingin memvalidasi surat ini?')">
-                                                                    Validasi
-                                                                </button>
-                                                            </form>
-                                                        @endif
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div class="d-flex justify-content-center my-4 pagination-wrapper">
-                                {{ $surat_keluars->appends(['search' => request()->input('search')])->links('pagination::bootstrap-4') }}
-                            </div>
-                        @else
-                            <div class="alert alert-info">
-                                Tidak ada data surat keluar ditemukan.
-                            </div>
-                        @endif
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="d-flex justify-content-center my-4 pagination-wrapper">
+                            {{ $surat_keluars->appends(['search' => request()->input('search')])->links('pagination::bootstrap-4') }}
+                        </div>
+                    @else
+                        <div class="alert alert-info">
+                            Tidak ada data surat keluar ditemukan.
+                        </div>
+                    @endif
                     </div>
                 </div>
             </main>
