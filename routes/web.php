50df437d67eb0246ee314f0f87d6ca498482be0d
Frrama <arjunafadli@gmail.com>
2025-02-13 13:39:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -248,6 +248,9 @@ Route::delete('surat_keluar/{surat_keluar}', [SuratKeluarController::class, 'des
 Route::resource('surat-keluar', SuratKeluarNewController::class);
 Route::get('surat-keluar/{id}/view', [SuratKeluarNewController::class, 'show'])->name('surat-keluar.view');
 Route::get('surat-keluar/create', [SuratKeluarController::class, 'create'])->name('surat-keluar.create');
+Route::get('surat-keluar/edit', [SuratKeluarController::class, 'create'])->name('surat-keluar.edit');
+Route::get('surat-keluar/{surat-keluar}/edit', [SuratKeluarController::class, 'edit'])->name('surat-keluar.edit');
+Route::put('surat-keluar/{surat-keluar}', [SuratKeluarController::class, 'update'])->name('surat-keluar.update');
 
 // Rute untuk resource Surat Keluar
 Route::resource('surat-keluar', SuratKeluarNewController::class);
