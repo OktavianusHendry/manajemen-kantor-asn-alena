39b116214059028555dcf38b9dfe48b5bff6640f
Frrama <arjunafadli@gmail.com>
2025-02-15 11:42:56 +0700

    update

--- resources/views/surat-keluar/view.blade.php
+++ resources/views/surat-keluar/view.blade.php
@@ -6,42 +6,49 @@
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Nomor Surat: <span class="text-primary">{{ $surat->nomor_surat }}</span></h5>
+            
             <div class="row mb-3">
                 <div class="col-md-6">
                     <p class="card-text"><strong>Tanggal Surat:</strong> {{ \Carbon\Carbon::parse($surat->tanggal_surat)->format('d-m-Y') }}</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 text-end">
                     <p class="card-text"><strong>Status Validasi:</strong> {{ $surat->status_validasi }}</p>
                 </div>
             </div>
+
             <div class="row mb-3">
                 <div class="col-md-6">
                     <p class="card-text"><strong>Perihal:</strong> {{ $surat->perihal }}</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 text-end">
                     <p class="card-text"><strong>Tujuan Surat:</strong> {{ $surat->tujuan_surat }}</p>
                 </div>
             </div>
+
             <div class="row mb-3">
                 <div class="col-md-6">
-                    <p class="card-text"><strong>Disahkan Oleh:</strong> {{ $surat->disahkan_oleh }}</p>
+                    <p class="card-text"><strong>Disahkan Oleh:</strong> {{ $surat->disahkan_oleh ?? 'Belum ada' }}</p>
                 </div>
-                <div class="col-md-6">
-                    <p class="card-text"><strong>Jabatan Pengesah:</strong> {{ $surat->jabatan_pengesah }}</p>
+                <div class="col-md-6 text-end">
+                    <p class="card-text"><strong>Jabatan Pengesah:</strong> {{ $surat->jabatan_pengesah ?? 'Belum ada' }}</p>
                 </div>
             </div>
+
             <div class="row mb-3">
                 <div class="col-md-6">
-                    <p class="card-text"><strong>Tembusan:</strong> {{ $surat->tembusan }}</p>
+                    <p class="card-text"><strong>Tembusan:</strong> {{ $surat->tembusan ?? 'Tidak ada tembusan' }}</p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 text-end">
                     @if($surat->lampiran)
                         <p class="card-text"><strong>Lampiran:</strong> 
                             <a href="{{ asset('path/to/lampiran/' . $surat->lampiran) }}" target="_blank" class="btn btn-info btn-sm">Lihat Lampiran</a>
                         </p>
+                    @else
+                        <p class="card-text"><strong>Lampiran:</strong> Tidak ada lampiran</p>
                     @endif
                 </div>
             </div>
+
             <div class="row mb-3">
                 <div class="col-md-12">
                     <p class="card-text"><strong>Isi Surat:</strong></p>
@@ -50,12 +57,15 @@
                     </div>
                 </div>
             </div>
+
             <div class="row mb-3">
                 <div class="col-md-6">
                     @if($surat->foto_surat)
                         <p class="card-text"><strong>Foto Surat:</strong> 
                             <a href="{{ asset('path/to/foto_surat/' . $surat->foto_surat) }}" target="_blank" class="btn btn-info btn-sm">Lihat Foto Surat</a>
                         </p>
+                    @else
+                        <p class="card-text"><strong>Foto Surat:</strong> Tidak ada foto surat</p>
                     @endif
                 </div>
                 <div class="col-md-6 text-end">
