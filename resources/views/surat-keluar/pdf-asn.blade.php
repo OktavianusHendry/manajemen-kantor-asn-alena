8c9fdcd0a021d480c540bd2b54deaed031bcf054
Frrama <arjunafadli@gmail.com>
2025-02-17 20:01:44 +0700

    update

--- resources/views/surat-keluar/pdf-asn.blade.php
+++ resources/views/surat-keluar/pdf-asn.blade.php
@@ -3,45 +3,83 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Surat Resmi</title>
+    <title>Surat Keluar</title>
     <style>
         @page {
-            size: A4;
-            margin: 120px 50px 100px 50px;
-        }
-
-        body {
-            font-family: Arial, sans-serif;
-            font-size: 12pt;
-            line-height: 1.5;
-            text-align: justify;
+            margin: 80px 50px; /* Set margins for the page */
         }
 
         .header {
             position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            text-align: center;
+            top: -50px; /* Position header */
+            left: -5px;
+            width: 45%;
+            text-align: left; /* Align header to the left */
         }
 
         .footer {
             position: fixed;
-            bottom: 0;
+            bottom: -10px; /* Position footer */
             left: 0;
+            right: 0;
             width: 100%;
             text-align: center;
         }
 
+        body {
+            font-family: Arial, sans-serif; /* Font Arial */
+            font-size: 12pt; /* Font size */
+            line-height: 1.5; /* Single spacing */
+            text-align: justify; /* Justify text */
+        }
+
         .content {
-            margin-top: 150px; /* Memberi ruang agar tidak tertutup header */
-            margin-bottom: 80px; /* Memberi ruang agar tidak tertutup footer */
-            page-break-after: auto;
+            margin-top: 50px; /* Margin from header to push content down */
+            margin-bottom: 70px; /* Ensure content does not overlap with footer */
+            text-align: justify; /* Justify text */
+        }
+
+        .bold {
+            font-weight: bold;
+        }
+
+        .underline {
+            text-decoration: underline;
         }
 
         .signature {
-            margin-top: 50px;
-            text-align: left;
+            margin-top: 50px; /* Margin for signature */
+            text-align: left; /* Align signature to the left */
+        }
+
+        .signature p {
+            margin: 0; /* Remove margin for signature paragraphs */
+        }
+
+        table {
+            width: 100%; /* Full width for table */
+            border-collapse: collapse; /* Remove space between borders */
+        }
+
+        td {
+            padding: 0px; /* Padding in table cells */
+            vertical-align: top; /* Align cells to the top */
+        }
+
+        .left-align {
+            text-align: left; /* Align text to the left */
+        }
+
+        .right-align {
+            text-align: right; /* Align text to the right */
+        }
+
+        .no-margin {
+            margin: 0; /* Remove margin */
+        }
+
+        .justify-align {
+            text-align: justify; /* Justify text */
         }
     </style>
 </head>
@@ -66,17 +104,24 @@
     </div>
     <!-- Isi Surat -->
     <div class="content">
-        <p>Nomor: <strong>{{ $surat->nomor_surat }}</strong></p>
-        <p>Tanggal: <strong>{{ $surat->tanggal_surat }}</strong></p>
-        <p>Kepada Yth,</p>
-        <p class="bold">{{ $surat->tujuan_surat }}</p>
-        <p>Di Tempat</p>
-        <br>
-        <p><strong>Perihal:</strong> <br> {{ $surat->perihal }}</p><br>
-        <p><strong>Lampiran:</strong> <br>{{ $surat->lampiran }}</p>
-        <br>
-        <p>{!! $surat->isi_surat !!}</p> 
-        <br>
+        <table>
+            <tr>
+                <td class="left-align">Nomor: <strong>{{ $surat->nomor_surat ?? 'Nomor tidak tersedia' }}</strong></td>
+                <td class="right-align"><strong>{{ \Carbon\Carbon::parse($surat->tanggal_surat)->translatedFormat('d F Y') }}</strong></td>
+            </tr>
+        </table> </br>
+        <p class="no-margin recipient">Kepada Yth,</p>
+        <p class="no-margin bold">{{ $surat->tujuan_surat }}</p>
+        <p class="no-margin recipient">Di</p>
+        <p class="no-margin">Tempat</p></br>
+
+        <p class="no-margin"><strong>Perihal:</strong></p>
+        <p class="no-margin"><strong>{{ $surat->perihal }}</strong></p></br>
+
+        <p class="no-margin"><strong>Lampiran:</strong></p>
+        <p class="no-margin">{{ $surat->lampiran }}</p>
+
+        <p class="no-margin justify-align">{!! $surat->isi_surat !!}</p> <!-- Justified content --> <br>
 
         <!-- Tanda Tangan 
         <div class="signature">
@@ -85,7 +130,7 @@
             <p class="no-margin">PT. Anagata Sisedu Nusantara</p>
             </br></br></br>
             <p class="no-margin">____________________</p> <!-- Signature line 
-            <p class="no-margin">{{ $surat->disahkan_oleh }}</p> <!-- Name of the signer 
+            <p class="no-margin">{{ $surat->disahkan_oleh }}</p> <!-- Name of the signer
         </div></br></br></br></br> --->
 
         <p class="no-margin"><strong>Tembusan:</strong></p>
