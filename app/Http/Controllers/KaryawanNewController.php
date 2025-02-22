d5fd947f10afe8bc866d9d585f8850584f90b1f0
Frrama <arjunafadli@gmail.com>
2025-02-22 18:19:26 +0700

    update karyawan

--- app/Http/Controllers/KaryawanNewController.php
+++ app/Http/Controllers/KaryawanNewController.php
@@ -7,11 +7,19 @@ use App\Models\User;
 
 class KaryawanNewController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $karyawan = User::where('role_as', 2)->with('biodata')->get();
+        $search = $request->input('search');
+
+        $karyawan = User::where('role_as', 2) // Role 2 untuk karyawan
+            ->when($search, function ($query, $search) {
+                return $query->where('name', 'LIKE', "%{$search}%");
+            })
+            ->paginate(10); // <-- Gunakan paginate() bukan get()
+
         return view('karyawan.index', compact('karyawan'));
     }
+
 
     public function show($id)
     {
