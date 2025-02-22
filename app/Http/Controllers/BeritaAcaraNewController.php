c616178b65036483f6470b7573f1c4636d9f525d
Frrama <arjunafadli@gmail.com>
2025-02-23 01:59:00 +0700

    update

--- app/Http/Controllers/BeritaAcaraNewController.php
+++ app/Http/Controllers/BeritaAcaraNewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\BeritaAcaraNew;
 use App\Models\PesertaBeritaAcara;
+use Illuminate\Support\Facades\Auth; // Tambahkan ini!
 use Illuminate\Http\Request;
 use App\Models\User;
 
