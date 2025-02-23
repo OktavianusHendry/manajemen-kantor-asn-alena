ccaec823ee3de9d4395cf3b044778a17ef0d4462
Frrama <arjunafadli@gmail.com>
2025-02-23 21:42:45 +0700

    update

--- app/Http/Controllers/MentorNewController.php
+++ app/Http/Controllers/MentorNewController.php
@@ -11,13 +11,19 @@ class MentorNewController extends Controller
     {
         $search = $request->input('search');
 
-        $mentor = User::where('role_as', 3) // Role 3 untuk mentor
+        $mentor = User::where('role_as', 0) // Role 0 untuk mentor
             ->when($search, function ($query, $search) {
                 return $query->where('name', 'LIKE', "%{$search}%");
             })
             ->paginate(10);
 
         return view('mentor.index', compact('mentor'));
+    }
+
+    public function show($id)
+    {
+        $karyawan = User::where('role_as', 0)->where('id', $id)->firstOrFail();
+        return view('mentor.show', compact('mentor'));
     }
 
 }
