b1894913be516db5f133ce26eeb90266ca46f11c
Frrama <arjunafadli@gmail.com>
2025-02-24 15:06:29 +0700

    update

--- resources/views/layoutss/sidebar.blade.php
+++ resources/views/layoutss/sidebar.blade.php
@@ -23,7 +23,7 @@
             </a>
         </li>
         <li class="menu-item">
-            <a href="{{ ## }}" class="menu-link">
+            <a href="{{ route('crew.dashboard') }}" class="menu-link">
                 <i class="bx bxs-user-account"></i>
                 <div data-i18n="Analytics">Biodata</div>
             </a>
